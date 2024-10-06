6d1307c999cf7c35d353c681edb973333cccbd46
Fabio Andrés Triana Rodriguez <124170936+Fabiotriana07@users.noreply.github.com>
2024-10-05 22:49:08 -0500

    Vista de mascotas desde el cliente

--- michito/src/app/tarjeta-mascota-cliente/tarjeta-mascota-cliente.component.ts
+++ michito/src/app/tarjeta-mascota-cliente/tarjeta-mascota-cliente.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Mascota } from '../Model/mascota';
+
 @Component({
   selector: 'app-tarjeta-mascota-cliente',
   standalone: true,
-  imports: [],
   templateUrl: './tarjeta-mascota-cliente.component.html',
-  styleUrl: './tarjeta-mascota-cliente.component.css'
+  styleUrls: ['./tarjeta-mascota-cliente.component.css']
 })
 export class TarjetaMascotaClienteComponent {
-
+  @Input() mascota!: Mascota;
+  @Output() detalles: EventEmitter<Mascota> = new EventEmitter<Mascota>();
+  verDetalles() {
+    console.log('Detalles de la mascota:', this.mascota);
+    this.detalles.emit(this.mascota);
+  }
 }
--- michito/src/app/vista-mascotas-cliente/vista-mascotas-cliente.component.ts
+++ michito/src/app/vista-mascotas-cliente/vista-mascotas-cliente.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { DetallesMascotaComponent } from '../detalles-mascota/detalles-mascota.component';
 import { TarjetaMascotaClienteComponent } from '../tarjeta-mascota-cliente/tarjeta-mascota-cliente.component';
 import { Mascota } from '../Model/mascota';
+import { CommonModule } from '@angular/common';
+import { Cliente } from '../Model/cliente';
+import { ClienteService } from '../Services/cliente.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-vista-mascotas-cliente',
   standalone: true,
-  imports: [DetallesMascotaComponent, TarjetaMascotaClienteComponent],
+  imports: [DetallesMascotaComponent, TarjetaMascotaClienteComponent, CommonModule],
   templateUrl: './vista-mascotas-cliente.component.html',
   styleUrls: ['./vista-mascotas-cliente.component.css']
 })
@@ -14,8 +18,34 @@ export class VistaMascotasClienteComponent {
   modoVer: boolean = false;
   mascotaSeleccionada!: Mascota;
   mascotas: Mascota[] = [];
+  clienteSeleccionado!: Cliente;
+  private ROOT_URL = 'http://localhost:8080/Clientes';
 
+  constructor (private http: HttpClient, private ClienteService: ClienteService) {}
   ngOnInit(): void {
+    console.log('cliente cargado:', this.ClienteService.getCliente());
+    this.clienteSeleccionado = this.ClienteService.getCliente()!;
     this.modoVer = false;
+    this.listarMascotas();
+  }
+
+  listarMascotas(): void {
+    this.http.get<Mascota[]>(`${this.ROOT_URL}/Mascotas/${this.clienteSeleccionado.id}`).subscribe({
+      next: (mascotas) => {
+        this.mascotas = mascotas;
+      },
+      error: (error) => {
+        console.error('Error al obtener las mascotas:', error);
+      }
+    });
+  }
+
+  onVerDetalles(mascota: Mascota) {
+    this.mascotaSeleccionada = mascota;
+    this.modoVer = true;  // Cambiamos a modo detalles
+  }
+
+  onVolverALista() {
+    this.modoVer = false;  // Cambiamos de vuelta al modo lista
   }
 }
