4c40b65e01c2d328aead5ab361351dda824afdbf
00CARLOSECG00 <caicedog.ce@javeriana.edu.co>
2024-10-14 12:24:32 -0500

    Ruta

--- michito/src/app/detalles-mascota/detalles-mascota.component.ts
+++ michito/src/app/detalles-mascota/detalles-mascota.component.ts
@@ -44,6 +44,6 @@ export class DetallesMascotaComponent {
   }
 
   onVolver(): void {
-    this.router.navigate(['/mascotas']); // Navega a la lista de mascotas
+    this.router.navigate(['/Mascotas']); // Navega a la lista de mascotas
   }
 }
