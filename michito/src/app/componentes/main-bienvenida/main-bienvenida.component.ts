fdb8ae04a59aaddd315b2e84ac5770192fc5d1da
Fabio Andrés Triana Rodriguez <124170936+Fabiotriana07@users.noreply.github.com>
2024-11-09 19:30:44 -0500

    Videos en el main

--- michito/src/app/componentes/main-bienvenida/main-bienvenida.component.ts
+++ michito/src/app/componentes/main-bienvenida/main-bienvenida.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import * as AOS from 'aos';
 
@@ -9,11 +9,17 @@ import * as AOS from 'aos';
   templateUrl: './main-bienvenida.component.html',
   styleUrl: './main-bienvenida.component.css'
 })
-export class MainBienvenidaComponent implements OnInit {
+export class MainBienvenidaComponent implements OnInit, AfterViewInit {
+  private videoSources = [
+    'videos/video1.mp4',
+    'videos/video2.mp4',
+    'videos/video3.mp4'
+  ];
+  private currentVideoIndex = 0;
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
-    // Solo inicializa AOS si estamos en el navegador
     if (isPlatformBrowser(this.platformId)) {
       AOS.init({
         offset: 120,
@@ -24,6 +30,31 @@ export class MainBienvenidaComponent implements OnInit {
         mirror: true,
         anchorPlacement: 'top-bottom',
       });
+    }
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const videoPlayer = document.getElementById('videoPlayer') as HTMLVideoElement;
+      
+      if (videoPlayer) {
+        // Configurar el primer video
+        videoPlayer.src = this.videoSources[this.currentVideoIndex];
+        
+        // Manejar el final de cada video
+        videoPlayer.addEventListener('ended', () => {
+          // Actualizar al siguiente índice de video
+          this.currentVideoIndex = (this.currentVideoIndex + 1) % this.videoSources.length;
+          
+          // Cambiar la fuente del video al siguiente
+          videoPlayer.src = this.videoSources[this.currentVideoIndex];
+          
+          // Reproducir el siguiente video
+          videoPlayer.play().catch(error => {
+            console.error('Error playing video:', error);
+          });
+        });
+      }
     }
   }
 }
